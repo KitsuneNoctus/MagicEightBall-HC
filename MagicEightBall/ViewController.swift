fcad1a51213e4e6852d7a8377bbb7aa06f09038d
KitsuneNoctus <theceintiant@gmail.com>
2019-11-16 21:04:50 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Henry Calderon on 11/16/19.
+//  Copyright © 2019 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
